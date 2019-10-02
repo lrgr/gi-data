16bd6e9d20b3f46d82fb12fab16b8bf9989fbfad
Jason Fan <jasonfan@cs.umd.edu>
2019-10-02 16:05:46 -0400

    collect processed datasets w/ snakemake and shell script for upload to objstore

--- create_chacon_contract.sh
+++
@@ -1,3 +0,0 @@
-echo outputs > processed_outputs.txt
-ls datasets/*/output/processed/* >> processed_outputs.txt
-chacon create -i processed_outputs.txt -o processed_file_md5_checksums.yml
---
+++ datasets/umobj_upload.sh
@@ -0,0 +1,24 @@
+if [ -z "$1" ]
+      then
+          echo "No release argument supplied"
+          echo "USAGE: sh <script> <release>"
+          exit
+fi
+
+
+target=lrgr:gi-data/$1
+echo "Deleting $target for clean upload"
+
+rmobj -r $target
+
+for d in \
+collins_et_al \
+roguev_et_al
+do
+    echo "Copying $d to ObjStore"
+    cpobj -r all_processed_datasets/$d lrgr:gi-data/$1
+done
+
+
+echo "Uploading metadata (git commit etc.) to ObjStore"
+cpobj all_processed_datasets/meta.txt lrgr:gi-data/$1/meta.txt
